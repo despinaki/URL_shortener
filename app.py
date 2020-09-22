04e4acf654fe16cf10abf35dcdfb3a8072eac142
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-09-22 17:09:35 +0100

    database setup changes

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
+# from database import URLs
+from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
 @app.route('/') # @<var-you-set-to-Flask(__name__)>
-def home(): 
+def home():
     return render_template('index.html')
 
-@app.route('/results', methods=['GET','POST']) 
+@app.route('/results', methods=['GET','POST'])
 def results():
     if request.method=='POST':
         user_url=request.form['url']
+        # URLs.query.all()
         return render_template('results.html', user_url=user_url)
     else:
         return render_template('results.html', user_url='somestring')
 
-app.run(debug=False)
+app.run(debug=True)
