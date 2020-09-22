f9aa0f8b23f61af09e6d8f87d6fb6034dffe8bed
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-09-22 17:31:35 +0100

    add URLs class and db config to the app.py

--- app.py
+++ app.py
@@ -4,9 +4,22 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 CORS(app)
 
-@app.route('/') # @<var-you-set-to-Flask(__name__)>
+
+class URLs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(200), nullable=False)
+    new_url = db.Column(db.String(80), nullable=True)
+
+    def __repr__(self):
+        return '<URLs %r>' % self.long_url
+
+
+
+@app.route('/') 
 def home():
     return render_template('index.html')
 
@@ -14,7 +27,10 @@ def home():
 def results():
     if request.method=='POST':
         user_url=request.form['url']
-        # URLs.query.all()
+        db.session.add(URLs(long_url=user_url))
+        db.session.commit()
+        check = URLs.query.all()
+        print(check)
         return render_template('results.html', user_url=user_url)
     else:
         return render_template('results.html', user_url='somestring')
